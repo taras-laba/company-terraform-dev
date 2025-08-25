6d9953e0fe091f299de648f9c7133621e4b141de
Taras Laba <taras.laba@gmail.com>
2025-08-25 15:15:10 +0300

    add init files

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+  default     = "taras-laba-dev"
+}
+
+variable "region" {
+  description = "The GCP region for resources"
+  type        = string
+  default     = "us-central1"
+}
+
