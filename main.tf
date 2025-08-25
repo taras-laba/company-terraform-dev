3ed6820435d0b703f12169dd5af9a942ccfa0a24
Taras Laba <taras.laba@gmail.com>
2025-08-25 17:36:01 +0300

    make changes

--- main.tf
+++ main.tf
@@ -19,22 +19,6 @@ resource "google_storage_bucket" "default" {
   }
 }
 
-resource "local_file" "default" {
-  file_permission = "0644"
-  filename        = "${path.module}/backend.tf"
-
-  # You can store the template in a file and use the templatefile function for
-  # more modularity, if you prefer, instead of storing the template inline as
-  # we do here.
-  content = <<-EOT
-  terraform {
-    backend "gcs" {
-      bucket = "${google_storage_bucket.default.name}"
-    }
-  }
-  EOT
-}
-
 resource "google_artifact_registry_repository" "company_repo" {
   location      = var.region
   repository_id = "company-repo"
